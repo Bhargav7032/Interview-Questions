b0ad1313f98c097420662dc3371158205e1f2224
Bhargav7032 <130824195+Bhargav7032@users.noreply.github.com>
2024-08-27 19:24:30 +0530

    Add files via upload

---
+++ InterviewQuestions/PrimeNumberDemo.java
@@ -0,0 +1,62 @@
+package com.demo;
+
+import java.util.Iterator;
+
+public class PrimeNumberDemo {
+public static void main(String[] args) {
+//		int num = 100; 
+//	    int count;  
+//
+//	    for (int i = 1; i <= num; i++) {
+//	      count = 0;  
+//
+//	      for (int j = 2; j <= i/2 ; j++) {
+//	        if (i % j == 0) {
+//	          count++;  
+//	          break;  
+//	        }
+//	      }
+//	      if (count == 0) {
+//	        System.out.println(i); 
+//	      }
+//	    }
+	
+//	 revsernumber
+//	for (int i = 10; i>=0; i--) {
+//		System.out.println(i);
+//	}
+	
+//	
+//for (int i = 1; i < 100; i++) {
+//	int count =0;
+//	for (int j = 2; j < i; j++) {
+//		if (i%j==0) {
+//         count++;
+//		}
+//		
+//	}
+//	if (count==0) {
+//		System.out.println(i);
+//}
+//	
+//	
+//		
+//	}	
+	
+//	int x=0;
+//	int xx=1;
+//	System.out.println(x);
+//	System.out.println(xx);
+//	for (int i = 0; i < 10; i++) {
+//	int num=x+xx;
+//	System.out.println(num);
+//	x=xx;
+//	xx=num;
+//	
+//}
+	
+}}
+
+	  
+
+
---
+++ InterviewQuestions/fibnaicDemo.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+public class fibnaicDemo {
+
+	public static void main(String[]args) {
+		int a=0;
+		int b=1;
+		System.out.println(a);
+		System.out.println(a);
+for (int i = 0; i <=10; i++) {
+	int num=a+b;
+	System.out.println(num);
+	 a=b;
+	 b=num;
+}
+	}}
